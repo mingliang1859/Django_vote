59d5fd372ef4a09249292b4dc4578fea770f3fe5
unknown <wangml01@chinaredstar.cn>
2021-05-26 16:30:43 +0800

    first commit"
    ;;;

---
+++ polls/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render, redirect
+
+from polls.models import Subject, Teacher
+
+
+# 学科页效果
+def show_subjects(request):
+    subjects = Subject.objects.all().order_by('no')
+    return render(request, 'subjects.html', {'subjects': subjects})
+
+
+def show_teachers(request):
+    try:
+        sno = int(request.GET.get('sno'))
+        teachers = []
+        if sno:
+            subject = Subject.objects.noly('name').get(no=sno)
+            teachers = Teacher.objects.filter(subject=subject).order_by('no')
+        return render(request, 'teachers.html',{
+                      'subject': subject,
+                      'teachers': teachers
+        })
+    except (ValueError, Subject.DoesNotExist):
+        return redirect('/')
